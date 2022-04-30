8c82975809bf6ee47b925d41db660144226e2575
José <75454584+Regulus01@users.noreply.github.com>
2022-04-30 13:13:02 -0300

    Testando API Rest

---
+++ Scaffold/RestWithASPNetUdemy/RestWithASPNetUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNetUdemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string firstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ Scaffold/RestWithASPNetUdemy/RestWithASPNetUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,67 @@
+﻿using RestWithASPNetUdemy.Model;
+
+namespace RestWithASPNetUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < persons.Count; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                id = IncrementAndGet(),
+                firstName = "Leandro",
+                LastName = "Costa",
+                Address = "Uberlandia - Minas Gerais - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                id = IncrementAndGet(),
+                firstName = "Person Name " + i,
+                LastName = "Person LastName " + i,
+                Address = "Some Address " + i,
+                Gender = i % 2 == 0 ? "Male" : "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+        
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
